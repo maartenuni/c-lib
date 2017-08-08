d76eb5113c2eb94834ef24d66303ca3b180c7f15
Maarten Duijndam <maartenuni@hotmail.com>
2017-08-08 21:43:21 +0200

    Add extra copyright info with year. Updates new-file.py

--- test/array_tests.c
+++ test/array_tests.c
@@ -1,19 +1,21 @@
 /*
-This file is part of c-lib
-
-c-lib is free software: you can redistribute it and/or modify
-it under the terms of the Lesser General Public License as published by
-the Free Software Foundation, either version 2.1 of the License, or
-(at your option) any later version.
-
-c-lib is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-Lesser General Public License for more details.
-
-You should have received a copy of the Lesser General Public License
-along with c-lib.  If not, see <http://www.gnu.org/licenses/>
-*/
+ * This file is part of c-lib
+ *
+ * Copyright © 2017 Maarten Duijndam
+ *
+ * c-lib is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * c-lib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the Lesser General Public License
+ * along with c-lib.  If not, see <http://www.gnu.org/licenses/>
+ */
 
 #include <CUnit/CUnit.h>
 #include <stdio.h>
--- util/new-file.py
+++ util/new-file.py
@@ -1,22 +1,41 @@
 #!/usr/bin/env python3
 
+import sys
+import os.path
+import datetime
+
 libname = 'c-lib'
 
 message = '''/*
-This file is part of {}
+ * This file is part of {}
+ *
+ * Copyright © {} {}
+ *
+ * {} is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * {} is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the Lesser General Public License
+ * along with {}.  If not, see <http://www.gnu.org/licenses/>
+ */'''
 
-{} is free software: you can redistribute it and/or modify
-it under the terms of the Lesser General Public License as published by
-the Free Software Foundation, either version 2.1 of the License, or
-(at your option) any later version.
+author = ""
+year = datetime.datetime.now().year
 
-{} is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-Lesser General Public License for more details.
+def print_usage():
+    bname = os.path.basename(sys.argv[0])
+    print("Usage: {} <Author Name>".format(bname), file=sys.stderr)
+    
 
-You should have received a copy of the Lesser General Public License
-along with {}.  If not, see <http://www.gnu.org/licenses/>
-*/'''
+if len(sys.argv) != 2:
+    print_usage()
+    exit(1)
 
-print (message.format(libname, libname, libname, libname));
+author = sys.argv[1]
+print (message.format(libname, year, author, libname, libname, libname));
